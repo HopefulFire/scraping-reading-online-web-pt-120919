190278f9fd87a5429e09079c6292f998e1ebc79a
Jade Lynn Masker <hopefulfire@gmail.com>
2020-01-27 11:59:40 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,4 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-260IBN").text
+puts doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b")
